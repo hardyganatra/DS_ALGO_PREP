273ea06b1e8d085fbac52bbd304fc05184bdcb5f
Hardik <hardikganatra20@gmail.com>
2021-05-29 20:00:58 +0530

    1 ) Insert Head node without disturbing other nodes

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,3 @@
-// initial setup of Node and LinkedList classes
 class Node {
 	constructor(data, next = null) {
 		this.data = data;
@@ -9,7 +8,17 @@ class LinkedList {
 	constructor() {
 		this.head = null;
 	}
+	// 1) Insert at Head Node
+	insertHead(data) {
+		let Newnode = new Node(data, this.head);
+		this.head = Newnode;
+	}
+}
 
-
-
-// //////////////////////////////////////////////////////////////////////////////////////////////
+let list = new LinkedList();
+let n1 = new Node(1);
+list.head = n1;
+list.insertHead(2);
+list;
+list.insertHead(3);
+list;
